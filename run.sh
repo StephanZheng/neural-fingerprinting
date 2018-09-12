6196106e308b93bdb3c8f68841db1ad0d084fec8
dathath <sdathath@caltech.edu>
2018-09-12 07:15:15 +0000

    Bug fixes

--- run.sh
+++ run.sh
@@ -81,7 +81,7 @@ mkdir -p $ADV_EX_DIR
 # Generate attacks
 if [ "$3" = "attack" ]; then
 
-for ATCK in 'fgsm'; do #  'all'; do
+for ATCK in 'all'; do #  'all'; do
 # Write out the different attacks, use 'all' to generate all attacks for same random subset of test
 # For now copy paste this list into eval_fingerprint :) Will figure out a fix later
 
